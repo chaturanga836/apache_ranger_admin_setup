3ec350f4d3eec94f5e0a4a1a87e1c26973d6709d
chathuranga836@gmail.com <chaturanga836@gmail.com>
2026-01-08 14:05:19 +0530

    lets try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,54 +1,23 @@
 #!/bin/bash
 set -e
 
-# 1. Path to the truststore you verified
 SYSTEM_CACERTS="/opt/java/openjdk/jre/lib/security/cacerts"
 
-# 2. Wait for Postgres
-until pg_isready -h ranger-db -p 5432 -U postgres; do
-  echo "Waiting for DB..."
-  sleep 2
-done
-
-# 3. Run Ranger Setup (This is where the overwrites happen)
+# 1. Run Setup
 if [ ! -f "${RANGER_HOME}/.setup_done" ]; then
-    echo "[I] Running Ranger setup.sh..."
     ${RANGER_HOME}/setup.sh
     touch ${RANGER_HOME}/.setup_done
 fi
 
-# 4. FIX THE CONFIGURATIONS (Do this AFTER setup.sh)
-echo "[I] Setup finished. Patching configurations to force LDAPS..."
+# 2. THE TRICK: Kill everything to ensure a clean slate
+echo "[I] Cleaning up any background processes from setup..."
+${RANGER_HOME}/ews/ranger-admin-services.sh stop || true
+pkill -f 'java' || true
+sleep 2
 
-# ADMIN_CONF="${RANGER_HOME}/conf/ranger-admin-site.xml"
-# SYNC_CONF="${RANGER_HOME}/conf/ranger-ugsync-site.xml"
-
-# update_prop() {
-#     local name=$1
-#     local value=$2
-#     local file=$3
-#     # Delete then add to ensure it's clean and correctly formatted
-#     xmlstarlet ed -L -d "//property[name='$name']" "$file"
-#     xmlstarlet ed -L -s "/configuration" -t elem -n "property" -v "" \
-#         -s "/configuration/property[last()]" -t elem -n "name" -v "$name" \
-#         -s "/configuration/property[last()]" -t elem -n "value" -v "$value" "$file"
-# }
-
-# for FILE in "$ADMIN_CONF" "$SYNC_CONF"; do
-#     if [ -f "$FILE" ]; then
-#         echo "Updating $FILE..."
-#         update_prop "ranger.ldap.url" "ldaps://ec2-65-0-150-75.ap-south-1.compute.amazonaws.com:636" "$FILE"
-#         update_prop "ranger.ldap.ssl.enabled" "true" "$FILE"
-#         update_prop "ranger.usersync.ldap.url" "ldaps://ec2-65-0-150-75.ap-south-1.compute.amazonaws.com:636" "$FILE"
-#         update_prop "ranger.usersync.ldap.sslEnabled" "true" "$FILE"
-#         # Force Ranger to look at the JRE truststore
-#         update_prop "ranger.usersync.truststore.file" "$SYSTEM_CACERTS" "$FILE"
-#     fi
-# done
-
-# 5. IMPORT THE CERTIFICATE (Last step before starting)
+# 3. IMPORT THE CERTIFICATE (Now that we know no Java is running)
 if [ -f "/opt/ranger/certs/ca.crt" ]; then
-    echo "[I] Importing ca.crt into $SYSTEM_CACERTS..."
+    echo "[I] Importing certificate..."
     chmod +w "$SYSTEM_CACERTS"
     keytool -delete -alias ldap-cert -keystore "$SYSTEM_CACERTS" -storepass changeit || true
     keytool -import -trustcacerts -noprompt \
@@ -58,10 +27,10 @@ if [ -f "/opt/ranger/certs/ca.crt" ]; then
         -storepass changeit
 fi
 
-# 6. START THE SERVICE
-echo "[I] Starting Ranger Admin..."
+# 4. START THE SERVICE FRESH
+echo "[I] Starting Ranger Admin Service (Fresh JVM)..."
 cd ${RANGER_HOME}/ews
 ./ranger-admin-services.sh start
 
-# Keep container alive and show logs
-tail -f ${RANGER_HOME}/logs/ranger-admin-*.log
+# Keep alive
+tail -f /opt/ranger-admin/logs/ranger-admin-*.log
